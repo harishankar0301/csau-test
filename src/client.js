2edf050b610aacd1cb1f055015b01e03b8d86ac0
harishankar0301 <harishankar0301@users.noreply.github.com>
2022-11-03 15:01:38 +0000

    deploy: 2fa58e35f3ec26d5d16a2a867155f3e15ecb7b00

--- src/client.js
+++
@@ -1,8 +0,0 @@
-import client from "@sanity/client"
-
-export default client({
-  projectId: "wzu06sd5",
-  dataset: "production",
-  useCdn: true,
-  apiVersion: "2022-09-21",
-});
--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-import Navbar from "./Navbar/Navbar";
-import Events from "./Events/Events";
-import Home from "./Home/Home"
-import Footer from "./Footer/Footer"
-
-export { Navbar, Events,Home,Footer};
